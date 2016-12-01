ee6c41408beab03a03743fb60cff1554d92367e9
Dagmara <daga.sz@hotmail.com>
2016-12-01 16:03:40 +0000

    Added updating sell_in, simplified the public interface, further refactored update quality

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -7,29 +7,29 @@ class GildedRose
     @conjured = ["Something conjured"]
   end
 
-  def update_quality()
+  def update()
     @items.each do |item|
-      if is_legendary?(item)
-        increase_quality(item, 1)
-        next
-      end
-      if is_expired?(item) || is_conjured?(item)
-        reduce_quality(item, 2)
-        item.quality = 0 if is_backstage_pass?(item)
-        next
-      end
-      if is_timeless?(item)
-        increase_quality(item, 1)
-        next
-      end
-      if is_backstage_pass?(item)
-        increase_quality(item, 2) if item.sell_in <= 10
-        increase_quality(item, 1) if item.sell_in <= 5
-      end
+      update_sell_in(item)
+      update_quality(item)
     end
   end
 
+
   private
+
+  def update_sell_in(item)
+    item.sell_in -= 1 unless is_legendary?(item)
+  end
+
+  def update_quality(item)
+    increase_quality(item, 1) if is_legendary?(item) || is_timeless?(item)
+    reduce_quality(item, 2) if is_conjured?(item) || is_expired?(item)
+    if is_backstage_pass?(item)
+      increase_quality(item, 2) if item.sell_in <= 10
+      increase_quality(item, 1) if item.sell_in <= 5
+      item.quality = 0 if is_expired?(item)
+    end
+  end
 
   def is_legendary?(item)
     @legendary.include?(item.name)
@@ -48,7 +48,7 @@ class GildedRose
   end
 
   def is_expired?(item)
-    item.sell_in == 0 && !is_timeless?(item)
+    item.sell_in <= 0 && !(is_timeless?(item) || is_legendary?(item))
   end
 
   def reduce_quality(item, amount)
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,68 +3,89 @@ require_relative '../item'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
+  describe "#update" do
+
+    it "does not change the item name" do
       items = [Item.new("foo", 3, 5)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update()
       expect(items[0].name).to eq "foo"
     end
 
-    it "reduces the quality twice faster once the sell by date has passed" do
-      items = [Item.new("foo", 0, 5)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 3
+    context "Update item sell_in" do
+      it "reduces the sell in date every day" do
+        items = [Item.new("foo", 3, 5)]
+        expect{
+          GildedRose.new(items).update()
+        }.to change {items[0].sell_in}.by(-1)
+      end
+      it "doesn't change the sell_in of legendary items (Sulfuras)" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 3, 5)]
+        expect{
+          GildedRose.new(items).update()
+        }.to change {items[0].sell_in}.by(0)
+      end
     end
 
-    it "never reduces quality of item below 0" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 0
-    end
+    context "Update item quality" do
 
-    it "increases the quality of Aged Brie as it gets older" do
-      items = [Item.new("Aged Brie", 6, 5)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 6
-    end
+      it "reduces the quality twice faster once the sell by date has passed" do
+        items = [Item.new("foo", 0, 5)]
+        expect{
+          GildedRose.new(items).update()
+        }.to change {items[0].quality}.by(-2)
+      end
 
-    it "never increases the quality of an item above 50" do
-      items = [Item.new("Aged Brie", 6, 50)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 50
-    end
+      it "never reduces quality of item below 0" do
+        items = [Item.new("foo", 0, 0)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 0
+      end
 
-    it "knows Sulfuras, being a legendary item, never decreases in quality" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 11
-    end
+      it "increases the quality of Aged Brie as it gets older" do
+        items = [Item.new("Aged Brie", 6, 5)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 6
+      end
 
-    it "knows Sulfuras, being a legendary item, never has to be sold" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 3, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 3
-    end
+      it "never increases the quality of an item above 50" do
+        items = [Item.new("Aged Brie", 6, 50)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 50
+      end
 
-    it "increases quality of Backstage Passes by 2 when there are 10 days or less left" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 12
-    end
+      it "knows Sulfuras, being a legendary item, never decreases in quality" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 10)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 11
+      end
 
-    it "increases quality of Backstage Passes by 3 when there are 5 days or less" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 13
-    end
+      it "knows Sulfuras, being a legendary item, never has to be sold" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 3, 10)]
+        GildedRose.new(items).update()
+        expect(items[0].sell_in).to eq 3
+      end
 
-    it "drops quality of Backstage Passes to 0 after the concert" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 0
-    end
+      it "increases quality of Backstage Passes by 2 when there are 10 days or less left" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 12
+      end
 
-    xit "degrades the quality of conjured items twice as fast as normal items" do
+      it "increases quality of Backstage Passes by 3 when there are 5 days or less" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 13
+      end
+
+      it "drops quality of Backstage Passes to 0 after the concert" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose.new(items).update()
+        expect(items[0].quality).to eq 0
+      end
+
+      xit "degrades the quality of conjured items twice as fast as normal items" do
+
+      end
 
     end
 
